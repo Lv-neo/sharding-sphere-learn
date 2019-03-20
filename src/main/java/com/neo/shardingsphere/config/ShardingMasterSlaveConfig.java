d2d2463e67a5ca0c0f495abcf58fdc24a117df07
neo <neo.shilong.xu@gmail.com>
2019-03-20 18:35:05 +0800

    第二种写法，拆分清晰

--- src/main/java/com/neo/shardingsphere/config/ShardingMasterSlaveConfig.java
+++ src/main/java/com/neo/shardingsphere/config/ShardingMasterSlaveConfig.java
@@ -1,6 +1,7 @@
 package com.neo.shardingsphere.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import io.shardingjdbc.core.api.algorithm.masterslave.MasterSlaveLoadBalanceAlgorithmType;
 import io.shardingjdbc.core.api.config.MasterSlaveRuleConfiguration;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,26 +29,27 @@ public class ShardingMasterSlaveConfig {
 
         // 配置主库
         DruidDataSource masterDataSource = new DruidDataSource();
-        masterDataSource.setDriverClassName(env.getProperty("sharding.jdbc.datasource.ds_master.driver-class-name"));
-        masterDataSource.setUrl(env.getProperty("sharding.jdbc.datasource.ds_master.jdbc-url"));
-        masterDataSource.setUsername(env.getProperty("sharding.jdbc.datasource.ds_master.username"));
-        masterDataSource.setPassword(env.getProperty("sharding.jdbc.datasource.ds_master.password"));
-        dataSources.put("ds_master", masterDataSource);
+        masterDataSource.setDriverClassName(env.getProperty("sharding.jdbc.datasource.ds-master.driver-class-name"));
+        masterDataSource.setUrl(env.getProperty("sharding.jdbc.datasource.ds-master.jdbc-url"));
+        masterDataSource.setUsername(env.getProperty("sharding.jdbc.datasource.ds-master.username"));
+        masterDataSource.setPassword(env.getProperty("sharding.jdbc.datasource.ds-master.password"));
+        dataSources.put("ds-master", masterDataSource);
 
         // 配置第一个从库
         DruidDataSource slaveDataSource1 = new DruidDataSource();
-        slaveDataSource1.setDriverClassName(env.getProperty("sharding.jdbc.datasource.ds_slave_0.driver-class-name"));
-        slaveDataSource1.setUrl(env.getProperty("sharding.jdbc.datasource.ds_slave_0.jdbc-url"));
-        slaveDataSource1.setUsername(env.getProperty("sharding.jdbc.datasource.ds_slave_0.username"));
-        slaveDataSource1.setPassword(env.getProperty("sharding.jdbc.datasource.ds_slave_0.password"));
-        dataSources.put("ds_slave_0", slaveDataSource1);
+        slaveDataSource1.setDriverClassName(env.getProperty("sharding.jdbc.datasource.ds-slave-0.driver-class-name"));
+        slaveDataSource1.setUrl(env.getProperty("sharding.jdbc.datasource.ds-slave-0.jdbc-url"));
+        slaveDataSource1.setUsername(env.getProperty("sharding.jdbc.datasource.ds-slave-0.username"));
+        slaveDataSource1.setPassword(env.getProperty("sharding.jdbc.datasource.ds-slave-0.password"));
+        dataSources.put("ds-slave-0", slaveDataSource1);
         return dataSources;
     }
 
     public MasterSlaveRuleConfiguration getMasterSlaveRule() {
         masterSlaveRule.setName("ds_master_slave");
-        masterSlaveRule.setMasterDataSourceName("ds_master");
-        masterSlaveRule.setSlaveDataSourceNames(Arrays.asList("ds_slave_0"));
+        masterSlaveRule.setMasterDataSourceName("ds-master");
+        masterSlaveRule.setSlaveDataSourceNames(Arrays.asList("ds-slave-0"));
+        masterSlaveRule.setLoadBalanceAlgorithmType(MasterSlaveLoadBalanceAlgorithmType.ROUND_ROBIN);
         return masterSlaveRule;
     }
 }
--- src/main/java/com/neo/shardingsphere/controller/UserController.java
+++ src/main/java/com/neo/shardingsphere/controller/UserController.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.neo.shardingsphere.entity.UserEntity;
 import com.neo.shardingsphere.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -15,14 +16,14 @@ public class UserController {
     @Resource
     private UserService userService;
 
-    @PostMapping("/save")
+    @RequestMapping("/save")
     public String saveUser() {
         UserEntity user = new UserEntity(1, "张三", 22);
         userService.saveUser(user);
         return "success";
     }
 
-    @PostMapping("/getUser")
+    @RequestMapping("/getUser")
     public String getUsers() {
         List<UserEntity> users = userService.getUsers();
         return new Gson().toJson(users);
