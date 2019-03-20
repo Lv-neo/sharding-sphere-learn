2a5104ec3904a2111f68670f2e4db1b8f69dca0b
neo <neo.shilong.xu@gmail.com>
2019-03-20 17:54:51 +0800

    更新到2.1.3

--- src/main/java/com/neo/shardingsphere/ShardingSphereApplication.java
+++ src/main/java/com/neo/shardingsphere/ShardingSphereApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication(scanBasePackages = {"com.neo"})
+@SpringBootApplication()
 public class ShardingSphereApplication {
 
 	public static void main(String[] args) {
